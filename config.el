7dddad4e312d57969314a3c5b15e348942949acb
Ps-Pencil <pan.song@dhs.sg>
2020-11-08 21:24:41 +0000

    Add failed attempt to skip a prompt when saving workspace.

--- config.el
+++ config.el
@@ -91,6 +91,10 @@
   :after '(evil-window-split evil-window-vsplit)
   (+ivy/switch-buffer))
 
+;; (defadvice! always-keep-others-when-saving-perspective (&optional fname phash name keep-others &rest rest-args)
+;;   :filter-args #'persp-save-to-file-by-names
+;;   '(fname phash name t rest-args))
+
 (map! :leader
       (:when (featurep! :ui workspaces)
        (:prefix-map ("TAB" . "workspace")
