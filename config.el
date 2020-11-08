72544b674b609abe8784daf645f10183da41ca6a
Ps-Pencil <pan.song@dhs.sg>
2020-11-08 17:09:38 +0000

    Use literate config, plus some tweaks:
    Mostly drawn from https://gitlab.com/justinekizhak/dotfiles/-/tree/master/emacs/doom.d

--- config.el
+++ config.el
@@ -1,98 +1,39 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;; config.el --- -*- lexical-binding: t -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Song Pan"
       user-mail-address "pan.song@dhs.sg")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
+(setq doom-theme 'doom-one)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light))
 
-;; -----------------------------------------------------------------------------
-;; New packages and configs
-;; -----------------------------------------------------------------------------
-(use-package! keyfreq
-  :config
-  (setq keyfreq-excluded-commands
-        '(self-insert-command
-          vterm--self-insert
-          org-self-insert-command
-          vterm-send-backspace
-          ))
-  (keyfreq-mode 1)
-  (keyfreq-autosave-mode 1))
-
-(use-package autorevert
+(use-package paren
   :defer t
   :ensure nil
+  :init (setq show-paren-delay 0.5)
+  :config (show-paren-mode +1))
+
+(use-package! org-bullets
   :config
-  (global-auto-revert-mode +1)
-  (setq auto-revert-interval 2
-        auto-revert-check-vc-info t
-        auto-revert-verbose nil))
-;; -----------------------------------------------------------------------------
-;; Better defaults
-;; -----------------------------------------------------------------------------
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 (after! evil-escape
   (setq evil-escape-key-sequence "fd"))
 
-(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
-      auto-save-default t)                        ; Nobody likes to loose work, I certainly don't
+(setq undo-limit 80000000)
 
-(global-subword-mode 1)                           ; Iterate through CamelCase words
+(global-subword-mode 1)
+
 (setq-default major-mode 'org-mode)
 
-;; -----------------------------------------------------------------------------
-;; Ivy related stuff
-;; -----------------------------------------------------------------------------
-(setq +ivy-buffer-preview t)                      ; Preview buffers in ivy
+(setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
 
-;; Make ivy-rich fallback to absolute path if not in a project.
+(setq +ivy-buffer-preview t)
+
 (after! ivy-rich
   (setq ivy-rich-path-style 'abbrev)
   (defadvice! ivy-rich-no-project-fallback (orig-fn candidate)
@@ -102,25 +43,36 @@
       (cons ""
             (expand-file-name (ivy-rich--switch-buffer-directory candidate))))))
 
-;; -----------------------------------------------------------------------------
-;; Workspace related stuff
-;; -----------------------------------------------------------------------------
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+
 (map! :leader
       (:when (featurep! :ui workspaces)
        (:prefix-map ("TAB" . "workspace")
         :desc "Switch to last workspace" "TAB" #'+workspace/other)))
 
-;; -----------------------------------------------------------------------------
-;; Magit related stuff
-;; -----------------------------------------------------------------------------
+(setq auto-save-default t)
 
-(setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
+(use-package! autorevert
+  :defer t
+  :ensure nil
+  :config
+  (global-auto-revert-mode +1)
+  (setq auto-revert-interval 2
+        auto-revert-check-vc-info t
+        auto-revert-verbose nil))
 
-;; -----------------------------------------------------------------------------
-;; Misc
-;; -----------------------------------------------------------------------------
-;; Prompt for buffer after splitting windows
-(defadvice! prompt-for-buffer (&rest _)
-  :after '(evil-window-split evil-window-vsplit)
-  (+ivy/switch-buffer))
+(use-package! files
+  :defer t
+  :config
+  (setq confirm-kill-processes nil))
 
+(defun xah-save-all-unsaved ()
+  "Save all unsaved files. no ask.
+Version 2019-11-05"
+  (interactive)
+  (save-some-buffers t))
+
+;; when switching out of emacs, all unsaved files will be saved
+(add-hook 'focus-out-hook 'xah-save-all-unsaved)
--- init.el
+++ init.el
@@ -37,7 +37,8 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       (ligatures         ; ligatures and symbols to make your code pretty again
+        +fira)
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -183,5 +184,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
