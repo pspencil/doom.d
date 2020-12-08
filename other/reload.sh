8cc2433cfac231ad21f23fda6a9fedfcdcf78afc
Ps-Pencil <pan.song@dhs.sg>
2020-12-08 13:28:57 +0000

    Allow reloading one file.

--- other/reload.sh
+++ other/reload.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -f ~/.emacs.d/bin/org-tangle ]]
+if [[ ! -f ~/.emacs.d/bin/org-tangle ]]
 then
-    ~/.emacs.d/bin/org-tangle ~/.doom.d/other
-else
     echo "Error: You must have doom emacs installed."
     exit 1
 fi
+
+if [[ "$#" = 0 ]]
+   then
+       ~/.emacs.d/bin/org-tangle ~/.doom.d/other
+else
+    ~/.emacs.d/bin/org-tangle "$1"
+fi
+
+    
