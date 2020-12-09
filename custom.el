ad238da055e38cdd669f9c4e1c172a494a0c73a0
Ps-Pencil <pan.song@dhs.sg>
2020-12-09 01:17:22 +0000

    Add local variable to auto-tangle on save.

--- custom.el
+++ custom.el
@@ -3,6 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+           (lambda nil
+             (org-babel-tangle))
+           nil t)))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
 (custom-set-faces
