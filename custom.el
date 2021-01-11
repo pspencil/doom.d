79a56dfbb90fc636c439973c7fcf378505462133
Song Pan <pan.song@dhs.sg>
2021-01-11 21:58:20 +0000

    Add some GTD related things.

--- custom.el
+++ custom.el
@@ -3,19 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval font-lock-add-keywords nil
-           `((,(concat "("
-                       (regexp-opt
-                        '("sp-do-move-op" "sp-do-move-cl" "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op" "sp-do-del-cl")
-                        t)
-                       "\\_>")
-              1 'font-lock-variable-name-face)))
-     (eval add-hook! 'after-save-hook :local #'org-babel-tangle)
-     (eval add-hook 'after-save-hook
-           (lambda nil
-             (org-babel-tangle))
-           nil t)))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
 (custom-set-faces
